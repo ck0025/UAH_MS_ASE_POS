e15322682ddd7e25f0e2bc78898cb20e282d9690
ck0025 <52710631+ck0025@users.noreply.github.com>
2026-04-12 12:32:10 -0400

    Separate file for the course list

---
+++ courses.js
@@ -0,0 +1,12 @@
+    const catalog = {
+        "MAE 530": "Fundamentals of Aerodynamics", "MAE 540": "Rocket Propulsion I", "MAE 541": "Airbreathing Propulsion",
+        "MAE 544": "Intro to Electric Propulsion", "MAE 555": "Process Control & Instrumentation", "MAE 560": "Introduction to Robotics",
+        "MAE 568": "Introduction to Orbital Mechanics", "MAE 585": "Design of Mech/Aero Systems", "MAE 623": "Computational Methods in MAE",
+        "MAE 627": "Engineering Systems", "MAE 631": "Boundary Layer Theory", "MAE 640": "Rocket Propulsion II",
+        "MAE 641": "Advanced Airbreathing Prop", "MAE 643": "Adv Liquid Rocket Prop", "MAE 650": "Intro to Controls Engine",
+        "MAE 652": "Advanced Flight Dynamics", "MAE 671": "Continuum Mechanics", "MAE 674": "Finite Element Analysis",
+        "MAE 692": "Graduate Engineering Analysis I", "MAE 693": "Graduate Engineering Analysis II", "MAE 780": "Adv Topics in Systems Eng",
+        "ISE 502": "Engineering Systems Dynamics", "ISE 526": "Design & Analysis of Exp", "ISE 627": "Engineering Systems",
+        "ISE 637": "Systems Modeling & Analysis", "ISE 638": "Engineering Reliability", "ISE 639": "Systems Integration",
+        "ISE 690": "Statistical Methods for Eng"
+    };
